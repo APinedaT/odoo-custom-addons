0a3b50b751606e7c56e21757313a9f886bdae648
Andres <apineda099@gmail.com>
2026-07-02 13:49:34 -0500

    feat: :sparkles: Add customer phone validation prompts in POS payment process

--- pi_pos_require_customer/static/src/app/utils/order_payment_validation.js
+++ pi_pos_require_customer/static/src/app/utils/order_payment_validation.js
@@ -5,15 +5,31 @@ import { ask } from "@point_of_sale/app/utils/make_awaitable_dialog";
 
 patch(OrderPaymentValidation.prototype, {
     async askBeforeValidation() {
-        if (this.pos.config.pi_require_customer && !this.order.getPartner()) {
-            const confirmed = await ask(this.pos.dialog, {
-                title: _t("Please select the Customer"),
-                body: _t("You must select a customer before validating this order."),
-            });
-            if (confirmed) {
-                this.pos.selectPartner();
+        if (this.pos.config.pi_require_customer) {
+            const partner = this.order.getPartner();
+            if (!partner) {
+                const confirmed = await ask(this.pos.dialog, {
+                    title: _t("Please select the Customer"),
+                    body: _t("You must select a customer before validating this order."),
+                });
+                if (confirmed) {
+                    this.pos.selectPartner();
+                }
+                return false;
             }
-            return false;
+            if (!partner.phone) {
+                const confirmed = await ask(this.pos.dialog, {
+                    title: _t("Missing Customer Phone"),
+                    body: _t(
+                        "The customer %s has no phone number. You must add one before validating this order.",
+                        partner.name
+                    ),
+                });
+                if (confirmed) {
+                    this.pos.selectPartner();
+                }
+                return false;
+            }
         }
         return await super.askBeforeValidation(...arguments);
     },
